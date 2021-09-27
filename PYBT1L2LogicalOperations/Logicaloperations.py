177f238c6473fa7cef07d393ac59b2887b754862
Jesse <Jkeppel19@gmail.com>
2021-09-27 12:40:43 +0200

    added files

---
+++ PYBT1L2LogicalOperations/Logicaloperations.py
@@ -0,0 +1,6 @@
+name = "Jesse"
+money = 2000
+wantIt = True
+
+if(name == "Jesse" and wantIt or money > 1000):
+    print("buy new clothes")
---
+++ hello you.py
@@ -0,0 +1,3 @@
+print ("hello you, ik ben jesse")
+username = input("Whats your name?")
+print("hello " + username)
